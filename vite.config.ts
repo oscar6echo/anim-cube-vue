576b0fa6f29a8cb78211ea95f12162d37bde9f25
Olivier Borderies <olivier.borderies@gmail.com>
2025-01-20 22:03:15 +0100

    Init

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'dev';
+  console.log({ mode, isDev });
+
+  return {
+    base: isDev ? '/dev/' : '/pages/oscar6echo/anim-cube-vue/',
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-})
+  };
+});
